045e101b3a34758f405f2a77dc3be2ce377ade5e
Jeremy Faden <faden@cottagesystems.com>
2022-04-29 08:02:15 -0500

    Update makeDeb.sh

--- build/makeDeb.sh
+++ build/makeDeb.sh
@@ -1,6 +1,10 @@
 echo "Building deb file from autoplot.jar"
 echo "This should be run in the root of the project."
 
+if [ ! -f dist/SmallJavaApp.jar ]; then
+    echo "dist/SmallJavaApp.jar does not exist"
+    exit(-1)
+
 /usr/local/jdk-17/bin/jpackage \
   --verbose \
   --input dist/ \
