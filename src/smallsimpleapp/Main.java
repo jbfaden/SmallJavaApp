d7b80962400fe8a96f233ebe3bad7b3aa40b2449
Jeremy Faden <faden@cottagesystems.com>
2023-02-28 08:51:42 -0600

    system LAF

--- src/smallsimpleapp/Main.java
+++ src/smallsimpleapp/Main.java
@@ -1,8 +1,11 @@
 
 package smallsimpleapp;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -10,6 +13,12 @@ import javax.swing.JOptionPane;
  */
 public class Main {
     public static void main( String[] args ) {
+        String s= javax.swing.UIManager.getSystemLookAndFeelClassName();
+        try {
+            javax.swing.UIManager.setLookAndFeel(s);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         JOptionPane.showConfirmDialog( null, new SmallSimpleApp(), "Rabbit Timer",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, new ImageIcon(Main.class.getResource("/icon.png")) );
     }
